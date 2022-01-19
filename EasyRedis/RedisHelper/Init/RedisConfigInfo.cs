a46cb614397a53f1438de4c5d8da5a1acdbf1748
tan1301230147 <1301230147@qq.com>
2022-01-19 15:27:47 +0800

    第一次上传

---
+++ EasyRedis/RedisHelper/Init/RedisConfigInfo.cs
@@ -0,0 +1,83 @@
+﻿using System.Configuration;
+
+namespace EasyRedis.Init
+{
+    /// <summary>
+    /// redis配置文件信息
+    /// 也可以放到配置文件去
+    /// </summary>
+    public static class RedisConfigInfo
+    {
+        /// <summary>
+        /// 可写的Redis链接地址
+        /// format:ip1,ip2
+        /// 默认6379端口
+        /// 
+        /// 如果有密码按下面格式填写
+        /// password@ip:port
+        /// </summary>
+        public static string WriteServerList = "";
+        /// <summary>
+        /// 可读的Redis链接地址
+        /// format:ip1,ip2
+        /// </summary>
+        public static string ReadServerList = "";
+        /// <summary>
+        /// 最大写链接数
+        /// </summary>
+        public static int MaxWritePoolSize = 60;
+        /// <summary>
+        /// 最大读链接数
+        /// </summary>
+        public static int MaxReadPoolSize = 60;
+        /// <summary>
+        /// 本地缓存到期时间，单位:秒
+        /// </summary>
+        public static int LocalCacheTime = 180;
+        /// <summary>
+        /// 自动重启
+        /// </summary>
+        public static bool AutoStart = true;
+        /// <summary>
+        /// 是否记录日志,该设置仅用于排查redis运行时出现的问题,
+        /// 如redis工作正常,请关闭该项
+        /// </summary>
+        public static bool RecordeLog = false;
+
+        ///// <summary>
+        ///// 可写的Redis链接地址
+        ///// format:ip1,ip2
+        ///// 默认6379端口
+        ///// 
+        ///// 如果有密码按下面格式填写
+        ///// password@ip:port
+        ///// </summary>
+        //public string WriteServerList = "124.223.85.193:6379";
+        ///// <summary>
+        ///// 可读的Redis链接地址
+        ///// format:ip1,ip2
+        ///// </summary>
+        //public string ReadServerList = "124.223.85.193:6379";
+        ///// <summary>
+        ///// 最大写链接数
+        ///// </summary>
+        //public int MaxWritePoolSize = 60;
+        ///// <summary>
+        ///// 最大读链接数
+        ///// </summary>
+        //public int MaxReadPoolSize = 60;
+        ///// <summary>
+        ///// 本地缓存到期时间，单位:秒
+        ///// </summary>
+        //public int LocalCacheTime = 180;
+        ///// <summary>
+        ///// 自动重启
+        ///// </summary>
+        //public bool AutoStart = true;
+        ///// <summary>
+        ///// 是否记录日志,该设置仅用于排查redis运行时出现的问题,
+        ///// 如redis工作正常,请关闭该项
+        ///// </summary>
+        //public bool RecordeLog = false;
+    }
+}
---
+++ EasyRedis/RedisHelper/Interface/RedisBase.cs
@@ -0,0 +1,85 @@
+﻿using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyRedis.Init;
+
+namespace EasyRedis.Interface
+{
+    /// <summary>
+    /// RedisBase类，是redis操作的基类，继承自IDisposable接口，主要用于释放内存
+    /// </summary>
+    public abstract class RedisBase : IDisposable
+    {
+        public IRedisClient IClient { get; private set; }
+        /// <summary>
+        /// 构造时完成链接的打开
+        /// </summary>
+        public RedisBase()
+        {
+            IClient = RedisManager.GetClient();
+        }
+
+        private bool _disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed)
+            {
+                if (disposing)
+                {
+                    IClient.Dispose();
+                    IClient = null;
+                }
+            }
+            this._disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Transcation()
+        {
+            using IRedisTransaction irt = IClient.CreateTransaction();
+            try
+            {
+                irt.QueueCommand(r => r.Set("key", 20));
+                irt.QueueCommand(r => r.Increment("key", 1));
+                irt.Commit(); // 提交事务
+            }
+            catch (Exception)
+            {
+                irt.Rollback();
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// 清除全部数据 请小心
+        /// </summary>
+        public virtual void FlushAll()
+        {
+            IClient.FlushAll();
+        }
+
+        /// <summary>
+        /// 保存数据DB文件到硬盘
+        /// </summary>
+        public void Save()
+        {
+            IClient.Save();//阻塞式save
+        }
+
+        /// <summary>
+        /// 异步保存数据DB文件到硬盘
+        /// </summary>
+        public void SaveAsync()
+        {
+            IClient.SaveAsync();//异步save
+        }
+    }
+}
